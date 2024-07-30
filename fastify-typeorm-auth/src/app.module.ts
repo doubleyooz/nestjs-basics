ba7863cf7811f24d3d99967000a6f49e26c038ad
Joanderson <sirjoandersonwf@gmail.com>
2024-07-29 21:53:27 -0300

    initial commit

---
+++ fastify-typeorm-auth/src/app.module.ts
@@ -0,0 +1,34 @@
+import { Module } from '@nestjs/common';
+
+import { ConfigModule } from '@nestjs/config';
+
+import * as Joi from 'joi';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { ItemsModule } from './items/items.module';
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        PORT: Joi.number().required(),
+        HASH_SALT: Joi.number().required(),
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRATION: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+        DB_TYPE: Joi.string().required(),
+      }),
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    ItemsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
